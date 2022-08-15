55976b7e014d7d5afd52b425053e9cd5fb5d4a59
Manassarn Manoonchai <manassarn@gmail.com>
2022-08-15 23:52:09 +0700

    Setup Prisma with Postgresql

--- src/lib/server/trpc.ts
+++ src/lib/server/trpc.ts
@@ -1,10 +1,16 @@
 import * as trpc from '@trpc/server';
 import type { inferAsyncReturnType } from '@trpc/server';
+import prismaClient from './prisma-client';
 
 export const createContext = async () => ({});
-export const router = trpc.router<inferAsyncReturnType<typeof createContext>>().query('hello', {
-	resolve: () => 'tRPC: Greetings!',
-});
+export const router = trpc
+	.router<inferAsyncReturnType<typeof createContext>>()
+	.query('hello', {
+		resolve: () => 'tRPC: Greetings!',
+	})
+	.mutation('createExample', {
+		resolve: () => prismaClient.example.create({ data: {} }),
+	});
 
 // only export *type signature* of router!
 // to avoid accidentally importing your API
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,13 +3,30 @@
 	import trpcClient from '$lib/client/trpc';
 
 	let greeting = 'Waiting for tRPC response...';
+	let id: String;
+	let loaded = false;
 
 	onMount(() => {
 		setTimeout(async () => {
 			greeting = await trpcClient.query('hello');
+			loaded = true;
 		}, 500);
 	});
+
+	async function createNewExample() {
+		const res = await trpcClient.mutation('createExample');
+		id = res.id;
+	}
 </script>
 
 <h1>Welcome to T3s</h1>
+
 <p>{greeting}</p>
+
+{#if loaded}
+	{#if id}
+		<p>ID: {id}</p>
+	{:else}
+		<p><button on:click={createNewExample}>Create new ID</button></p>
+	{/if}
+{/if}
